4a51ac37cbee779eb7b39c8730d506e34ad79f17
LFabreu <lfabreu1707@gmail.com>
2024-04-16 20:20:58 -0300

    aula 7

--- CY3_terca/Aula_7/sessao.php
+++ CY3_terca/Aula_7/sessao.php
@@ -2,29 +2,27 @@
  // INICIAR UM SESSÃO 
 session_start();
 
-
-
-
-
  // VERIFICAR SE O USUÁRIO JÁ ESTÁ LOGADO 
 if (isset($_SESSION['user']))
 {
     $nome_usuario = $_SESSION['user'];
+    header('Location: painel.php');
 
 } else {
 
-    if(isset($_POST['user'])) 
+    if(isset($_POST['user']) && isset($_POST['password'])) 
     {
         $nome_usuario = $_POST['user'];
+        $senha_usuario = $_POST['password'];
         // SIMULA VERIFICAÇÃO NO BD
-        if(!empty($nome_usuario))
+        if(!empty($nome_usuario) && !empty($senha_usuario))
         {
             $_SESSION['user'] = $nome_usuario;
-            echo 'Login bem-sucedido!! Bem vindo' . $nome_usuario;
+            header("Location: painel.php");
 
         } else {
 
-            echo 'Por favor, insira um nome de usuário';
+            header("Location: index.php");
             
         }
     }
